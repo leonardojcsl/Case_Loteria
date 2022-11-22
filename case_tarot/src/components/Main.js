33d89f8feabd28ad50cbaab124c0eb474ff5a06c
Leonardo Couto <leonardojcsl@gmail.com>
2022-11-22 15:54:03 -0300

    Funcionamento de cards e imagens

--- case_tarot/src/components/Main.js
+++ case_tarot/src/components/Main.js
@@ -1,20 +1,35 @@
 import { useState } from "react";
 import cardsList from "./cardsList";
+import styled from "styled-components";
+
+const MainDiv = styled.div `
+  border: 2px solid red;
+  display: flex;
+  flex-direction: column;
+`
+
+const CardDiv = styled.div`
+    border: 1px solid black;
+    display: flex;
+`
 
 const Main = () => {
   const [cards, setCards] = useState(cardsList);
   
-  const cardsNames = cards?.map((e) => {
+  const cardsInfos = cards?.map((e) => {
     return (
-    <div>
+    <CardDiv>
       <p>{e.name}</p>
-    </div>
-    
+      <img src={e.image}/>
+      <p>{e.meaning }</p>
+    </CardDiv>    
     )
-  })
+  }) 
 
   return (
-    <div>{cardsNames}</div>
+    <MainDiv>
+    {cardsInfos}  
+    </MainDiv>
   )
 }
 
